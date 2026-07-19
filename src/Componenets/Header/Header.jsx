212a02ad247e78470bea5363db6e9e85edc0b1fb
Mahliyo <gasandigital13@gmail.com>
2026-07-19 21:05:08 +0500

    Portfolio aboutPages

--- src/Componenets/Header/Header.jsx
+++ src/Componenets/Header/Header.jsx
@@ -2,7 +2,13 @@ import Link from 'next/link'
 import React from 'react'
 function Header() {
   return (
-    <div>Header</div>
+    <>
+    <header>
+        <Link href="/">About</Link>
+        <Link href="/">About</Link>
+        <Link href="/">About</Link>
+    </header>
+    </>
   )
 }
 
